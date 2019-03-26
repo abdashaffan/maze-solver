aafbd3c544ab54fb1e36438994e7ddaa325ec1f8
Abda Shaffan Diva <abdashaffan@gmail.com>
2019-03-26 15:02:22 +0700

    Edit read text implementation

--- GUI.py
+++ GUI.py
@@ -2,34 +2,20 @@
 # Abda Shaffan D 13517021
 # Juniardi Akbar 13517075
 
-import turtle as T  #kakas yang digunakan untuk menggambar maze
 
-wn = T.Screen()
-wn.bgcolor("black")
-wn.title("Maze Solver using BFS and A* algorithm")
-wn.setup(700, 700)
-
-
-class Pen(T.Turtle):
-    def __init__(self):
-        self.shape("square")
-        self.color("white")
-        self.penup()
-        self.speed(0)
-
-
+#Program utama
 def main():
-    # Parse maze data from text file
+
+    #Menyimpan data matrix dari text file
     with open("maze-sm.txt", "r") as f:
         data = f.readlines()
 
     matrix = []
-    for line in data:
-        newLine = []
-        for letter in line:
-            if (letter != '\n'):
-                newLine.append(int(letter))
-        matrix.append(newLine)
+
+    for row in data:
+        #di akhir text harus ada newline
+        matrixRow = row[:-1]
+        matrix.append(matrixRow)
     print(matrix)
 
 
