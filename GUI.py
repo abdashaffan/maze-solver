0c4765f9e169fdc198e8da2daf9672b649eecd9b
Abda Shaffan Diva <abdashaffan@gmail.com>
2019-03-27 16:17:31 +0700

    implement BFS

--- GUI.py
+++ GUI.py
@@ -1,7 +1,8 @@
 
 import turtle
 import os
-
+from bfs import *
+import queue
 
 '''
 BAGIAN PENGATURAN GUI
@@ -11,8 +12,6 @@ PEN_COLOR = 'white'
 BLOCK_SIZE = 24
 
 # Kelas dari modul turtle yang dipakai untuk menggambar maze
-
-
 class Pen(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
@@ -23,15 +22,39 @@ class Pen(turtle.Turtle):
         self.speed(0)
 
 
-class Point():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+class Cell():
+    def __init__(self, point, parentPoint, symbol):
+        self.point = Point(point.x, point.y)
+        self.parentPoint = Point(parentPoint.x, parentPoint.y)
+        self.isWall = (symbol == '0')
+
+
+# Fungsi pembuat maze
+def initMaze(maze, windowHeight, windowWidth, startPoint, finishPoint):
+
+    pen = Pen()
+
+    for x in range(len(maze)):
+        for y in range(len(maze[0])):
+            p = [x,y]
+            block = maze[x][y]
+
+            # Setting supaya mazenya ada di tengah window
+            setPosition(pen, p, windowHeight, windowWidth)
+
+            if (x == startPoint.x and y == startPoint.y):
+                pen.color('red')
+                pen.stamp()
+            elif (x == finishPoint.x and y == finishPoint.y):
+                pen.color('blue')
+                pen.stamp()
+            elif block == '1':
+                pen.color(PEN_COLOR)
+                pen.stamp()
+
 
 # Asumsi: pada maze hanya terdapat 1 entrace dan 1 exit
-
-
-def setEntraceExit(maze):
+def setEntraceExit(maze, startPoint, finishPoint):
     row = len(maze)
     col = len(maze[0])
     p1 = []
@@ -69,73 +92,44 @@ def setEntraceExit(maze):
         xFinish = p1[0]
         yFinish = p1[1]
 
-    # Set titik start dan finish
-    temp1 = list(maze[xStart])
-    temp1[yStart] = 'S'
-    maze[xStart] = ''.join(temp1)
-
-    temp2 = list(maze[xFinish])
-    temp2[yFinish] = 'F'
-    maze[xFinish] = ''.join(temp2)
+    # pass startPoint and finishPoint
+    startPoint.x = xStart
+    startPoint.y = yStart
+    finishPoint.x = xFinish
+    finishPoint.y = yFinish
 
     return maze
 
-
+#Set posisi pen pada posisi window tempat penggambaran akan dilakukan
 def setPosition(pen, point, winHeight, winWidth):
 
-    scr_x = -winWidth/2 + point.y*BLOCK_SIZE + 0.5*BLOCK_SIZE
-    scr_y = winHeight/2 - point.x*BLOCK_SIZE - 0.5*BLOCK_SIZE
+    scr_x = -winWidth/2 + point[1]*BLOCK_SIZE + 0.5*BLOCK_SIZE
+    scr_y = winHeight/2 - point[0]*BLOCK_SIZE - 0.5*BLOCK_SIZE
 
     pen.goto(scr_x, scr_y)
 
-
+#Menggambar 1 petak path pada turtle
 def drawPath(pen, point, winHeight, winWidth):
     pen.color('green')
-    setPosition(pen, point.x, point.y, winHeight, winWidth)
+    setPosition(pen, point, winHeight, winWidth)
     pen.stamp()
 
-
+#Menghapus 1 petak path pada turtle
 def erasePath(pen, point, winHeight, winWidth):
     pen.color(BG_COLOR)
-    setPosition(pen, point.x, point.y, winHeight, winWidth)
+    setPosition(pen, point[0], point[1], winHeight, winWidth)
     pen.stamp()
 
 
-def isWall(maze, point):
-    return (maze[point.x][point.y] == '0')
-
-# Fungsi pembuat maze
-
-
-def initMaze(maze, windowHeight, windowWidth):
-
+def animatePath(path, winHeight, winWidth):
     pen = Pen()
-
-    for x in range(len(maze)):
-        for y in range(len(maze[0])):
-            p = Point(x, y)
-            block = maze[x][y]
-
-            # Setting supaya mazenya ada di tengah window
-            setPosition(pen, p, windowHeight, windowWidth)
-
-            # Gambar tembok
-            if block == 'S':
-                pen.color('red')
-                pen.stamp()
-            elif block == 'F':
-                pen.color('green')
-                pen.stamp()
-            elif block == '1':
-                pen.color(PEN_COLOR)
-                pen.stamp()
+    for point in path:
+        drawPath(pen, point, winHeight, winWidth)
 
 
 '''
 BAGIAN ALGORITMA PENYELESAIAN
 '''
-
-
 def manhattanDistance(p1, p2):
     return (abs(p1.x-p2.x) + abs(p1.y-p2.y))
 
@@ -143,8 +137,6 @@ def manhattanDistance(p1, p2):
 '''
 PROGRAM UTAMA
 '''
-
-
 def main():
 
     # Input nama file
@@ -167,8 +159,51 @@ def main():
             mazeRow = row
         maze.append(mazeRow)
 
-    maze = setEntraceExit(maze).copy()
+    startPoint = Point()
+    finishPoint = Point()
 
+    maze = setEntraceExit(maze, startPoint, finishPoint).copy()
+
+    start = [startPoint.x, startPoint.y]
+    finish = [finishPoint.x, finishPoint.y]
+
+
+    ############### BIKIN 1 FUNGSI SENDIRI DI bfs.py ###############
+    arrived = []
+    for i in range(len(maze)):
+        arr = []
+        for j in range(len(maze[0])):
+            arr.append(False)
+        arrived.append(arr)
+
+    q = queue.Queue()
+    q.put(start)
+    found = False
+    solusi = []
+    while (not(q.empty()) and not(found)):
+        found = BFS(q, finish, arrived, solusi, maze)
+
+    res = [solusi[0]]
+    solusi.pop(0)
+
+    while (len(res) > 0):
+        path = res.pop(0)
+
+        node = path[-1]
+        if (node == finish):
+            break
+
+        for i in solusi:
+            if (isTetangga(node, i[0])):
+                new_path = list(path)
+                for j in i:
+                    new_path.append(j)
+                res.append(new_path) 
+
+    #mengembalikan path hasil pencarian, akan digunakan pada animasi
+    ############### BIKIN 1 FUNGSI SENDIRI DI bfs.py ###############
+    
+    #Set ukuran window
     windowHeight = len(maze)*BLOCK_SIZE
     windowWidth = len(maze[0])*BLOCK_SIZE
 
@@ -182,7 +217,9 @@ def main():
     wn.title("Maze solver dengan BFS dan A*")
 
     # Membuat maze
-    initMaze(maze, windowHeight, windowWidth)
+    initMaze(maze, windowHeight, windowWidth, startPoint, finishPoint)
+    #Menggambar path final pada GUI
+    animatePath(path,windowHeight,windowWidth)
 
     wn.exitonclick()
     # turtle.done()
--- bfs.py
+++ bfs.py
@@ -1,8 +1,9 @@
 import queue as queue
-from colored import fg, bg, attr
 
-def CekTetangga(start, arrived):
-    global matriks, brs, kol
+
+def CekTetangga(start, arrived, matriks):
+    brs = len(matriks)
+    kol = len(matriks[0])
     b = start[0]
     k = start[1]
     count = 0
@@ -20,25 +21,28 @@ def CekTetangga(start, arrived):
             count = count+1
     return count
 
-def getTetangga(start, arrived):
-    global matriks, brs, kol
+
+def getTetangga(start, arrived, matriks):
+    brs = len(matriks)
+    kol = len(matriks[0])
     b = start[0]
     k = start[1]
     res = []
     if (b-1 >= 0):
         if (matriks[b-1][k] == 0 and not(arrived[b-1][k])):
-            res.append([(b-1),k])
+            res.append([(b-1), k])
     if (b+1 < brs):
         if (matriks[b+1][k] == 0 and not(arrived[b+1][k])):
-            res.append([(b+1),k])
+            res.append([(b+1), k])
     if (k-1 >= 0):
         if (matriks[b][k-1] == 0 and not(arrived[b][k-1])):
-            res.append([b,(k-1)])
+            res.append([b, (k-1)])
     if (k+1 < kol):
         if (matriks[b][k+1] == 0 and not(arrived[b][k+1])):
-            res.append([b,(k+1)])
-    
+            res.append([b, (k+1)])
+
     return res
+
 
 def isTetangga(A, B):
     if (A[0] == B[0]):
@@ -57,98 +61,37 @@ def isTetangga(A, B):
             return False
     else:
         return False
-    
 
-def BFS(q,akhir,arrived,solusi):
-    global matriks, brs, kol
+
+def BFS(q, akhir, arrived, solusi, maze):
+    brs = len(maze)
+    kol = len(maze[0])
+    matriks = []
+    for i in range(brs):
+        M = []
+        for j in range(kol):
+            M.append(int(maze[i][j]))
+        matriks.append(M)
     start = q.get()
     arrived[start[0]][start[1]] = True
-    tetangga = CekTetangga(start, arrived)
+    tetangga = CekTetangga(start, arrived, matriks)
     jalur = [start]
 
     while (tetangga == 1):
-        next = getTetangga(start, arrived)[0]
-        tetangga = CekTetangga(next, arrived)
+        next = getTetangga(start, arrived, matriks)[0]
+        tetangga = CekTetangga(next, arrived, matriks)
         start = next
         arrived[start[0]][start[1]] = True
         jalur.append(start)
-    
+
     if (start == akhir):
         solusi.append(jalur)
         return True
     else:
         if (tetangga == 0):
-            # solusi.append(jalur)
             return False
         else:
-            for i in getTetangga(start, arrived):
+            for i in getTetangga(start, arrived, matriks):
                 q.put(i)
             solusi.append(jalur)
             return False
-
-# MAIN PROGRAM
-
-text = input()
-text = text.split()
-
-brs = int(text[0])
-kol = int(text[1])
-matriks = []
-arrived = []
-for i in range(brs):
-    m = []
-    arr = []
-    text = input()
-    for j in range(kol):
-        arr.append(False)
-        el = int(text[j])
-        m.append(el)
-    arrived.append(arr)
-    matriks.append(m)
-
-text = input()
-text = text.split()
-awal = [int(text[0]), int(text[1])]
-
-text = input()
-text = text.split()
-akhir = [int(text[0]), int(text[1])]
-
-q = queue.Queue()
-q.put(awal)
-found = False
-solusi = []
-while (not(q.empty()) and not(found)):
-    found = BFS(q,akhir,arrived,solusi)
-
-idx = 0
-res = [solusi[0]]
-solusi.pop(0)
-
-# print(res)
-# print(solusi)
-
-while (len(res)>0):
-    path = res.pop(0)
-    
-    node = path[-1]
-    # print(path)
-    if (node == akhir):
-        break
-
-    for i in solusi:
-        if (isTetangga(node, i[0])):
-            new_path = list(path)
-            for j in i:
-                new_path.append(j)
-            res.append(new_path)
-
-print(path)
-# for i in range(brs):
-#     for j in range(kol):
-#         temp = [i,j]
-#         if (temp in path):
-#             print('%s%s%d%s' % (fg(16),bg(5),matriks[i][j],attr(0)) ,end="")
-#         else:
-#             print(matriks[i][j], end="")
-#     print()
